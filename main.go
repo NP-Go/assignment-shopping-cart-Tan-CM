80011c1527a2c1a10ef0e162d206f0a250d60d48
Tan-CM <tanchorming@gmail.com>
2022-04-01 16:56:31 +0800

    latest change

--- main.go
+++ main.go
@@ -86,23 +86,6 @@ func main() {
 	}
 }
 
-// View Shopping List
-func viewShoppingList(shopListMap map[string]shopItem, categoryMap map[int]string) {
-	// show all shopping items category, item , qty and unit price
-	// Get all key of a map of struct
-	keys := make([]string, 0, len(shopListMap))
-	for k, _ := range shopListMap {
-		keys = append(keys, k)
-	}
-	//	fmt.Println(keys)
-
-	for _, shopItem := range keys {
-		fmt.Printf("Category: %s - Item: %s Quantity: %d Unit Cost: %.0f\n",
-			shopItem, categoryMap[shopListMap[shopItem].category], shopListMap[shopItem].qty, shopListMap[shopItem].cost)
-	}
-
-}
-
 func genShoppingReport(shopListMap map[string]shopItem, categoryMap map[int]string) {
 	generateReport := []string{
 		"Generate Report",
